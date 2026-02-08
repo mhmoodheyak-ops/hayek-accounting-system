e2c55a4f2f5dd3c9ae69c916136b1d80b7260c72
mhmoodheyak-ops <mhmoodheyak@gmail.com>
2026-02-09 02:15:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 // config.js
-export const SUPABASE_URL = "https://itidwqyrjydmegjzuvn.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_j4uBD1htJvuMvOWUKC9w7g_mwVQzHb_";
-
-// إعدادات PDF (تقدر تغيرها لاحقاً بسهولة)
-export const COMPANY_NAME_AR = "شركة الحايك";
-export const APP_NAME = "HAYEK SPOT";
-export const WHATSAPP_NUMBER = "05510217646";
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://itidwqvyrjydmegjzuvn.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_j4ubD1htJvuMvOWUKC9w7g_mwVQzHb_"
+};
